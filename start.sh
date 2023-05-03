ec7aa6a2aa4f70ec2de7724729cb5c0f9ea7e7b7
Donald Gray <donald.gray@digirati.com>
2023-05-03 17:21:37 +0100

    Allow http port to be specified by envvar

--- start.sh
+++ start.sh
@@ -8,6 +8,6 @@ RELOAD_VCL=1
 
 mkdir -p ${VARNISH_CACHE_FOLDER}
 
-varnishd -a 0.0.0.0:80 -T 127.0.0.1:6082 -f /etc/varnish/default.vcl -s file,${VARNISH_CACHE_FOLDER}/varnish_cache.bin,${VARNISH_CACHE_SIZE}
+varnishd -a 0.0.0.0:${VARNISH_PORT} -T 127.0.0.1:6082 -f /etc/varnish/default.vcl -s file,${VARNISH_CACHE_FOLDER}/varnish_cache.bin,${VARNISH_CACHE_SIZE}
 
 varnishlog
